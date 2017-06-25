c9c392a404e5f684bc5750f718e7a40a19722731
Ferdinand Zickner <ferdinandlzickner.de>
2017-06-25 07:41:36 +0200

    Create a log file for each startup.

--- fotobox.sh
+++ fotobox.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 PATH_FOTOBOX="/home/pi/git/fotobox"
+LOG_DATE"date +%y%m%d-%H%M"
 cd $PATH_FOTOBOX
 mkdir -p $PATH_FOTOBOX/log
 
-sudo python $PATH_FOTOBOX/lib/led_server.py > $PATH_FOTOBOX/log/led_server.log 2> $PATH_FOTOBOX/log/led_server.err &
+sudo python $PATH_FOTOBOX/lib/led_server.py > $PATH_FOTOBOX/log/led_server_$LOG_DATE.log 2> $PATH_FOTOBOX/log/led_server_$LOG_DATE.err &
 PID=$!
 
-python $PATH_FOTOBOX/fotobox.py > $PATH_FOTOBOX/log/fotobox.log 2> $PATH_FOTOBOX/log/fotobox.err
+python $PATH_FOTOBOX/fotobox.py > $PATH_FOTOBOX/log/fotobox_$LOG_DATE.log 2> $PATH_FOTOBOX/log/fotobox_$LOG_DATE.err
 RETURN=$?
 
 sudo kill $PID
