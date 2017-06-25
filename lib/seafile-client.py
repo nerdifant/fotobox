0e31707c12b3a994c58cf96a453dbd176bcb583b
Ferdinand Zickner <ferdinandlzickner.de>
2017-06-25 13:19:54 +0200

    Status query for seafile client added.

--- lib/seafile-client.py
+++ lib/seafile-client.py
@@ -4,17 +4,20 @@ import urllib
 import urllib2
 import json
 import ssl
+import ccnet
+import seafile
 
 class SeaFile:
     def __init__(self, config):
         self.url = config['url']
         self.auth = config['auth']
+        self.conf_dir = config['conf_dir']
         self.context = ssl._create_unverified_context()
 
         # Post seafile email/password to obtain auth token.
         data = urllib.urlencode(self.auth)
         try:
-            self.token = self._api2("auth-token/", data)['token']
+            self.token = json.loads(self.urlopen("auth-token/", data))['token']
         except:
             print("[Error]: Connection to seafile server failed.")
 
@@ -27,27 +30,28 @@ class SeaFile:
 
         return None
 
-    def _api2(self, path, data = None, reqType = False):
+    def urlopen(self, path, data = None, reqType = False):
         req = urllib2.Request(self.url + "/api2/" + path, data)
         if hasattr(self, 'token'): req.add_header('Authorization', 'Token ' + self.token)
         if reqType: req.get_method = lambda: reqType
-
-        try:
-            response = urllib2.urlopen(req, context = self.context)
-        except:
-            return False
-
-        page = response.read()
-        if page == "":
-            return response.info()
-        else:
-            return json.loads(page)
+        try:    resp = urllib2.urlopen(req, context = self.context)
+        except: return False
+        return resp.read()
 
     def ping(self):
-        return "pong" == self._api2("auth/ping/")
+        return "pong" == json.loads(self.urlopen("auth/ping/"))
+
+    def status(self):
+        pool = ccnet.ClientPool(self.conf_dir)
+        seafile_rpc = seafile.RpcClient(pool, req_pool=False)
+        t = seafile_rpc.get_repo_sync_task(self.repo["id"])
+        if not t:
+            return "waiting for sync"
+        else:
+            return t.state
 
     def _get_repo(self, name):
-        repos =  self._api2("repos/")
+        repos = json.loads(self.urlopen("repos/"))
         for repo in repos:
             if repo["name"] == name:
                 return repo
@@ -59,10 +63,10 @@ class SeaFile:
         if self.shared_link_expire > 0: values["expire"] = self.shared_link_expire    # days
         if self.shared_link_password: values["password"] = self.shared_link_password
         data = urllib.urlencode(values)
-        self._api2("repos/" + self.repo["id"] + "/file/shared-link/", data, "PUT")
+        self.urlopen("repos/" + self.repo["id"] + "/file/shared-link/", data, "PUT")
 
         ## Get Link
-        fileshares = self._api2("shared-links/")["fileshares"]
+        fileshares = json.loads(self.urlopen("shared-links/"))["fileshares"]
         for f in fileshares:
             if f["repo_id"] == self.repo["id"] and f["path"] == path_file:
                 return self.url + "/f/" + f["token"]
@@ -76,6 +80,7 @@ def main():
             # Test Seafile
             seafile = SeaFile(seaf_conf)
             if seafile.ping():
+                print seafile.status()
                 print seafile.get_share_link("/2017-06-24/pic00004.jpg")
         return 0
     else:
